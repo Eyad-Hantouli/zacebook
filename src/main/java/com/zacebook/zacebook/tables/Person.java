13a8e11f6a669f097c0c8a0191d23175735c4df0
Eyad-Hantouli <eyadhantouli0@gmail.com>
2023-12-02 16:54:04 +0300

    Builder pattern on Person.java class

--- src/main/java/com/zacebook/zacebook/tables/Person.java
+++ src/main/java/com/zacebook/zacebook/tables/Person.java
@@ -7,6 +7,7 @@ import java.time.LocalDate;
 @Entity
 @Table(name="PERSON")
 public class Person {
+    // -- Fields Section --
     // PK
     @Id
     @Column(name="user_name")
@@ -27,16 +28,22 @@ public class Person {
     @JoinColumn(name = "active_profile_picture_id", referencedColumnName = "id")
     private ProfilePicture activeProfilePicture;
 
+    // -- Constructors Section --
     public Person() {
     }
 
-    public Person(String userName, String email, String firstName, String lastName, String password) {
-        this.userName = userName;
-        this.email = email;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.password = password;
+    public Person(PersonBuilder builder) {
+        this.userName = builder.userName;
+        this.email = builder.email;
+        this.firstName = builder.firstName;
+        this.lastName = builder.lastName;
+        this.password = builder.password;
+        this.dob = builder.dob;
+        this.address = builder.address;
+        this.activeProfilePicture = builder.activeProfilePicture;
     }
+
+    // -- Getter & Setters Section --
 
     public String getUserName() {
         return userName;
@@ -100,5 +107,51 @@ public class Person {
 
     public void setActiveProfilePicture(ProfilePicture activeProfilePicture) {
         this.activeProfilePicture = activeProfilePicture;
+    }
+
+    // -- Builder Section --
+
+    public static class PersonBuilder {
+        private final String userName;
+        private final String email;
+        private final String firstName;
+        private final String lastName;
+        private final String password;
+        private LocalDate dob;
+        private String address;
+        private ProfilePicture activeProfilePicture;
+
+        public PersonBuilder(String userName, String email, String firstName, String lastName, String password) {
+            this.userName = userName;
+            this.email = email;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.password = password;
+        }
+
+        public PersonBuilder setDob(LocalDate dob) {
+            this.dob = dob;
+            return this;
+        }
+
+        public PersonBuilder setAddress(String address) {
+            this.address = address;
+            return this;
+        }
+
+        public PersonBuilder setActiveProfilePicture(ProfilePicture activeProfilePicture) {
+            this.activeProfilePicture = activeProfilePicture;
+            return this;
+        }
+
+        public Person build() {
+            Person person = new Person(this);
+            validPerson(person);
+            return person;
+        }
+
+        private void validPerson(Person person) {
+            // some validation logic of person object before initiate it
+        }
     }
 }
